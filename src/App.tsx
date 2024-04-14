acf27dc4a04ca62ef15e1633a6cce303acdfaff5
Saad Rawasia <saad.rawasia15@gmail.com>
2024-04-14 22:06:08 +0200

    fix: add setTimeout before alert and fix the index value of wordSplit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { generateGrid } from "./helpers";
 
 function App() {
-  const word = "clone".split("");
+  const word = "brave".split("");
   const wordLength = word.length;
   const [attempts, setAttempts] = useState(generateGrid(wordLength));
   const [submittedAttempts, setSubmittedAttempts] = useState(
@@ -52,16 +52,14 @@ function App() {
         const wordSplit = JSON.parse(JSON.stringify(word));
         for (let i = 0; i < wordLength; i++) {
           if (word[i] === attempts[currentAttempt][i]) {
-            console.log("yes");
             newSubmittedAttempts[currentAttempt][i] = "same";
-            const index = wordSplit.indexOf(wordSplit[i]);
+            const index = wordSplit.indexOf(attempts[currentAttempt][i]);
             wordSplit.splice(index, 1);
           } else if (wordSplit.includes(attempts[currentAttempt][i])) {
             newSubmittedAttempts[currentAttempt][i] = "diff";
             const index = wordSplit.indexOf(attempts[currentAttempt][i]);
             wordSplit.splice(index, 1);
           } else {
-            console.log("yes3");
             newSubmittedAttempts[currentAttempt][i] = "no-match";
           }
         }
@@ -70,11 +68,15 @@ function App() {
         setCurrentAttempt(currentAttempt + 1);
 
         if (attempts[currentAttempt].join("") === word.join("")) {
-          alert("success");
-          reset();
+          setTimeout(() => {
+            alert("success");
+            reset();
+          }, 10);
         } else if (currentAttempt === 5) {
-          alert("fail");
-          reset();
+          setTimeout(() => {
+            alert("success");
+            reset();
+          }, 10);
         }
       }
     };
